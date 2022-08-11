3704aafd7465d49eb21c555da8a9a6cd3b2fdb3b
Enrique Moreno <morenocarmonaenrique@gmail.com>
2022-08-11 10:01:12 +0200

    Component compound listo

---
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,39 @@
+import {
+  ProductImage,
+  ProductButtons,
+  ProductCard,
+  ProductTitle,
+} from "../components";
+
+const product = {
+  id: "1",
+  title: "Coffee mug - Card",
+  img: "./coffee-mug.png",
+};
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>ShoppingPage</h1>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        <ProductCard product={product}>
+          <ProductImage />
+          <ProductTitle />
+          <ProductButtons />
+        </ProductCard>
+        <ProductCard product={product}>
+          <ProductCard.Image />
+          <ProductCard.Title title={"Hola mundo"} />
+          <ProductCard.Buttons />
+        </ProductCard>
+      </div>
+    </div>
+  );
+};
