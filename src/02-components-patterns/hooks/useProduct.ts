e41c33fd373989bf169915b78270bad2c2ab1359
Enrique Moreno <morenocarmonaenrique@gmail.com>
2022-08-12 16:37:28 +0200

    Control Props alternativo

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -15,12 +15,8 @@ export const useProduct = ({
   
   const [counter, setCounter] = useState(value);
   
-  const isControlled = useRef( !!onChange )
   
   const increaseBy = (value: number) => {
-    if(isControlled.current) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
